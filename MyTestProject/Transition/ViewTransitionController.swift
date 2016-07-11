31ac9a0a5bec8c13237b1163fa36c81dd829f92b
tomatobin <tomatobin@163.com>
2016-07-11 22:58:02 +0800

    增加View Transition

--- MyTestProject/Transition/ViewTransitionController.swift
+++ MyTestProject/Transition/ViewTransitionController.swift
@@ -10,37 +10,73 @@ import UIKit
 
 class ViewTransitionController: FPBaseController {
     
-    @IBOutlet weak var stepOneView: UILabel!
-    @IBOutlet weak var stepTwoView: UILabel!
+    @IBOutlet weak var onNextBtn: UIButton!
+    var stepOneView: UILabel!
+    var stepTwoView: UILabel!
     
-    let rightFrame: CGRect = CGRect(x: FP_SCREEN_WIDTH, y: 200, width: 100, height: 40)
-    let centerFrame: CGRect = CGRect(x: (FP_SCREEN_WIDTH - 100)/2.0, y: 200, width: 100, height: 40)
-    let leftFrame: CGRect = CGRect(x: -100, y: 200, width: 100, height: 40)
+    let rightFrame: CGRect = CGRect(x: FP_SCREEN_WIDTH, y: 200, width: 200, height: 60)
+    let centerFrame: CGRect = CGRect(x: (FP_SCREEN_WIDTH - 200)/2.0, y: 200, width: 200, height: 60)
+    let leftFrame: CGRect = CGRect(x: -200, y: 200, width: 200, height: 60)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.fp_darkBgColor()
-        
-        self.stepOneView.frame = rightFrame
-        self.stepTwoView.frame = rightFrame
+        self.initUIWidgets()
         
         self.showView(stepOneView)
     }
     
+    func initUIWidgets() {
+        stepOneView = UILabel(frame: self.rightFrame)
+        stepOneView.text = "第一步，我要从右边显示出来，从左边隐藏"
+        stepOneView.numberOfLines = 0
+        stepOneView.font = UIFont.systemFontOfSize(17)
+        stepOneView.textColor = UIColor.whiteColor()
+        self.view.addSubview(stepOneView)
+        
+        stepTwoView = UILabel(frame: self.rightFrame)
+        stepTwoView.numberOfLines = 0
+        stepTwoView.text = "第二步，我要从右边显示出来，从左边隐藏"
+        stepTwoView.font = UIFont.systemFontOfSize(17)
+        stepTwoView.textColor = UIColor.whiteColor()
+        self.view.addSubview(stepTwoView)
+        
+        self.onNextBtn.alpha = 0
+    }
+    
     func showView(view: UIView) {
-        view.frame = rightFrame
-        UIView.transitionWithView(self.stepOneView, duration: 0.3, options: .TransitionFlipFromLeft, animations: {
+        view.alpha = 0.5
+        UIView.animateWithDuration(0.6, delay: 0, options: .CurveEaseIn, animations: {
+            view.alpha = 1
             view.frame = self.centerFrame
-            }, completion: { _ in
+            self.onNextBtn.alpha = 1
+        }, completion: {_ in
+        })
+    }
+    
+    @IBAction func onNextAction(sender: AnyObject) {
+        self.showSecondView()
+    }
+    
+    func showSecondView() {
+        UIView.animateWithDuration(0.6, delay: 0, options: .CurveEaseIn, animations: {
+            self.stepOneView.alpha = 0
+            self.stepOneView.frame = self.leftFrame
+            self.onNextBtn.alpha = 0
+        }, completion: {_ in
+            UIView.animateWithDuration(0.6, delay: 0, options: .CurveEaseIn, animations: {
+                self.stepTwoView.frame = self.centerFrame
+                self.onNextBtn.alpha = 1
+            }, completion: {_ in
+                    
+            })
         })
     }
     
     func hideView(view: UIView) {
         
     }
-}
-
-extension ViewTransitionController{
+    
     //MARK: 解决导航栏手势滑动过程中不显示的问题
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
