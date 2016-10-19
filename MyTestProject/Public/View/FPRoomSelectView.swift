18de0ac9776bb8f03738a45b69dd56c6ad43daa4
tomatobin <tomatobin@163.com>
2016-10-19 09:33:10 +0800

    房间选择完善

--- MyTestProject/Public/View/FPRoomSelectView.swift
+++ MyTestProject/Public/View/FPRoomSelectView.swift
@@ -11,11 +11,16 @@ class FPRoomSelectView: UIView {
     /// Room格数组
     var gridInfo = Array<FPRoomGridInfo>()
     
+    var actualLine = Int(2)
+    
     /// 单格的左右间距
-    private var gridPadding = CGFloat(10)
+    private var gridPadding = CGFloat(5)
     
     /// 单格的宽度及高度
     private var gridWidth = CGFloat(37), gridHeight = CGFloat(38)
+    
+    /// 格视图数组
+    private var gridsView = Array<FPRoomGridView>()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -28,7 +33,7 @@ class FPRoomSelectView: UIView {
     }
     
     func buildTestData() {
-        for index in 0 ..< 18 {
+        for index in 0 ..< 21 {
             let gridInfo = FPRoomGridInfo()
             gridInfo.number = index + 1
             self.gridInfo.append(gridInfo)
@@ -49,48 +54,53 @@ class FPRoomSelectView: UIView {
         for subview in self.subviews {
             subview.removeFromSuperview()
         }
+        self.gridsView.removeAll()
         
         let countInLine = self.gridsCountInLine()
         let totalLine = self.gridsLines()
         
         for line in 0 ..< totalLine {
             let yOffset = CGFloat(line) * self.gridHeight + 15 * CGFloat(line + 1)
-            var frame = CGRect(x: 0, y: yOffset, width: self.gridWidth, height: self.gridHeight)
+            var frame = CGRect(x: self.gridPadding, y: yOffset, width: self.gridWidth, height: self.gridHeight)
             
-            if line % 2 == 0 { //对应第1行、3、5等
-                for index in 0 ..< countInLine {
-                    let roomIndex = 2*index
-                    frame = CGRectOffset(frame, self.gridWidth - 1, 0)
-                    let gridView = FPRoomGridView(frame: frame)
-                    gridView.gridInfo = self.gridInfo[roomIndex]
-                    gridView.gridInfo.gridDirection = .Down
-                    gridView.gridInfo.gridState = .Selected
-                    gridView.updateRoomGrid()
-                    self.addSubview(gridView)
+            for index in 0 ..< countInLine {
+                var roomIndex = 0
+                if line % 2 == 0 {
+                    roomIndex = 2*index + line * countInLine //对应第01、03、05等
+                } else {
+                    roomIndex = (line - 1) * (countInLine - 1) + 2*index + 1 //对应02、04、06等
                 }
-            } else {
-                for index in 0 ..< countInLine {
-                    let roomIndex = 2*index + 1
-                    frame = CGRectOffset(frame, self.gridWidth - 1, 0)
-                    let gridView = FPRoomGridView(frame: frame)
-                    gridView.gridInfo = self.gridInfo[roomIndex]
-                    gridView.gridInfo.gridDirection = .Up
-                    gridView.updateRoomGrid()
-                    self.addSubview(gridView)
+                if roomIndex >= self.gridInfo.count {
+                    continue
                 }
+                
+                let gridView = FPRoomGridView(frame: frame)
+                gridView.gridInfo = self.gridInfo[roomIndex]
+                gridView.gridInfo.gridDirection = (line % 2 == 0) ? .Down : .Up
+                gridView.updateRoomGrid()
+                self.addSubview(gridView)
+                self.gridsView.append(gridView)
+                
+                frame = CGRectOffset(frame, self.gridWidth - 1, 0)
             }
         }
     }
     
     /**
      总的行数
-     
      - returns: Int
      */
     func gridsLines () -> Int {
         let countInLine = self.gridsCountInLine()
-        let result = self.gridInfo.count / countInLine
-        return Int(result)
+        let result = Float(self.gridInfo.count) / Float(countInLine)
+        
+        //刚好可以显示下
+        if result == Float(Int(result)) {
+            return Int(result)
+        }
+        
+        //显示不完：需要对称2行显示
+        return 2*Int(result)
     }
     
     /**
@@ -98,7 +108,7 @@ class FPRoomSelectView: UIView {
      */
     func gridsCountInLine() -> Int {
         let result = (FP_SCREEN_WIDTH - 2*self.gridPadding) / self.gridWidth
-        return Int(result)
+        return Int(result + 0.5)
     }
 
 }
