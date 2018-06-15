0f04b909693e1f5498139704caf3f8f16868d11f
tomatobin <tomatobin@163.com>
2018-06-15 11:41:43 +0800

    tmp

--- MyTestProject/Common/Widgets/AddDevice/DHApGuideViewController.swift
+++ MyTestProject/Common/Widgets/AddDevice/DHApGuideViewController.swift
@@ -21,15 +21,12 @@ class DHApGuideViewController: DHGuideBaseViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+	//MARK: DHGuideBaseVCProtocol
+	override func tipImageName() -> String? {
+		return "adddevice_failhrlp_default"
+	}
+	
+	override func tipImageUrl() -> String? {
+		return ""
+	}
 }
